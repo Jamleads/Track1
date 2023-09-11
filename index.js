f6662366ee862c83b9c731493dc4335d42230900
AdetunjiJakes <68664742+Jamleads@users.noreply.github.com>
2023-09-11 12:03:38 +0100

    completed

--- index.js
+++ index.js
@@ -8,13 +8,3 @@ currentDay.textContent = dayOfTheWeek;
 const timeUTC = date.toUTCString().split(" ")[4];
 
 currentTime.textContent = timeUTC;
-
-// Mon, 11 Sep 2023 10:39:49 GMT
-
-// function Day() {
-//   const date = new Date();
-//   const dayOfWeek = date.toLocaleDateString("en-US", { weekday: "long" });
-//   return dayOfWeek;
-// }
-
-// currentDay.textContent = Day();
